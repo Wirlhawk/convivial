74852e2ee8848fd12fe4ae4964d5e8776f87ab1e
Wirlhawk <rayhantogarlumbantobing@gmail.com>
2025-12-09 21:45:53 +0700

    feat

--- web/src/components/landing/curve-text.tsx
+++ web/src/components/landing/curve-text.tsx
@@ -55,7 +55,7 @@ export default function TextPathScroll() {
     return (
         <div
             ref={container}
-            className="h-[300vh] relative pt-[40vh] bg-[url('/assets/wth-bg/5.png')] bg-[size:100%_auto] bg-repeat-y"
+            className="h-[500vh] relative pt-[40vh] bg-[url('/assets/wth-bg/2.svg'),url('/assets/wth-bg/5.png')] [background-position:bottom,center] [background-size:100%_auto,100%_auto] [background-repeat:no-repeat,repeat-y]"
         >
             <div className="flex justify-center items-center sticky top-1/2 -translate-y-1/2 text-primary ">
                 <svg className="w-full pt-30" viewBox="0 0 250 150">
